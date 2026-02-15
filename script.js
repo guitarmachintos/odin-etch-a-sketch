2dd030c42cb54bfabf4dbfe0bf082394466a2f7b
guitarmachintos <guitarmachintos@gmail.com>
2026-02-15 12:46:53 +0700

    Finish basic function and elements

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const gridSlider = document.querySelector('#gridSlider');
 const sketchContainer = document.querySelector('.sketchContainer');
 const sliderValue = document.querySelector('#sliderValue');
+const pencilColorPicker = document.querySelector('#pencilColorPicker');
+const backgroundColorPicker = document.querySelector('#backgroundColorPicker');
 
 sliderValue.textContent = gridSlider.value;
 
@@ -13,26 +15,56 @@ createGrid(totalPixel, pixelSize);
 
 gridSlider.addEventListener('input', () => {
     sliderValue.textContent = gridSlider.value;
+});
+
+gridSlider.addEventListener('mouseup', () => {
     sketchContainer.textContent = '';
-    // createGrid(sketchContainer.offsetWidth);
-    // const sketchPixel = document.createElement('div');
-    // sketchPixel.style.border = '1px solid black';
-    // sketchPixel.style.width = '100' +'px';
-    // sketchContainer.appendChild(sketchPixel);
     totalPixel = gridSlider.value;
     pixelSize = 100 / gridSlider.value;
     createGrid(totalPixel, pixelSize);
 });
 
 function createGrid(totalPixel, pixelSize) {
-    for (let i = 0; i < totalPixel; i++) {
-        for (let j = 0; j < totalPixel; j++) {
-            const sketchPixel = document.createElement('div');
-            sketchPixel.style.border = '1px solid black';
-            sketchPixel.style.width = pixelSize + '%';
-            sketchPixel.style.height = pixelSize + '%';
-            sketchContainer.appendChild(sketchPixel);
-        }        
+    for (let i = 0; i < totalPixel * totalPixel; i++) {
+        const sketchPixel = document.createElement('div');
+        sketchPixel.style.border = '1px solid black';
+        sketchPixel.style.width = pixelSize + '%';
+        sketchPixel.style.height = pixelSize + '%';
+        // sketchPixel.id = 'pixel' + i;
+        sketchContainer.appendChild(sketchPixel);
     }
 }
+
+let primaryMouseButtonDown = false;
+
+function setPrimaryButtonState(e) {
+    if ('buttons' in e) {
+        primaryMouseButtonDown = (e.buttons & 1) === 1;
+    } else if ('which' in e) {
+        primaryMouseButtonDown = e.which === 1;
+    } else {
+        // last fallback: button is 0=left,1=middle,2=right
+        primaryMouseButtonDown = e.button === 0;
+    }
+}
+
+document.addEventListener("mousedown", setPrimaryButtonState);
+document.addEventListener("mouseup", setPrimaryButtonState);
+
+sketchContainer.addEventListener('mouseover', (e) => {
+    if(!primaryMouseButtonDown){
+        return;
+    }
+    if(e.target.classList.contains('sketchContainer')){
+        return;
+    }
+    e.target.style.backgroundColor = 'black';
+});
+
+sketchContainer.addEventListener('click', (e) => {
+    if(e.target.classList.contains('sketchContainer')){
+        return;
+    }
+    e.target.style.backgroundColor = 'black';
+});
 
